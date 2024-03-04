2e2dc866c257b3c956ba2020c1d2e16f418c89c7
MGD2003D <angrymailru@gmail.com>
2024-03-04 21:10:45 +0300

    minor changes

--- src/Application/habrbr.Application.Models/User.cs
+++ src/Application/habrbr.Application.Models/User.cs
@@ -8,7 +8,7 @@ public class User
     public Guid ID { get; set; }
     public string UserName { get; set; }
     public string Email { get; set; }
-    public string Password { get; set; } // Предполагается, что пароль уже зашифрован
+    public string Password { get; set; } // Предполагается, что пароль захеширован
     public DateTime RegistrationDate { get; set; }
     public virtual ICollection<Blog> Blogs { get; set; } = new List<Blog>();
 }
--- src/Application/habrbr.Application.Models/UserRightsInBlog.cs
+++ src/Application/habrbr.Application.Models/UserRightsInBlog.cs
@@ -8,6 +8,6 @@ public class UserRightsInBlog
     public Guid ID { get; set; }
     public virtual User User { get; set; }
     public virtual Blog Blog { get; set; }
-    public string UserRole { get; set; } // Можно использовать enum для определения ролей
+    public string UserRole { get; set; }
     public DateTime RightsAssignmentDate { get; set; }
 }
